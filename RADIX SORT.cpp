42457beb7c29c72d70ce0695c4e8bb131cde3fba
KULDIP MOCHI <53702326+kulu123-z@users.noreply.github.com>
2019-09-17 02:04:39 +0530

    Update RADIX SORT.cpp

--- RADIX SORT.cpp
+++ RADIX SORT.cpp
@@ -5,6 +5,54 @@ void radixSort(int [], int);
 int getMax(int [], int);
 void countSort(int [], int , int);
 void printArray(int [], int);
+/*
+final checked it is working program
+int largest(int a[])  
+{     
+    int larger=a[0], i;   
+    for(i=1;i<10;i++)  
+    {  
+        if(a[i]>larger)  
+        larger = a[i];  
+    }  
+    return larger;  
+}  
+void radix_sort(int a[])  
+{  
+    int bucket[10][10], bucket_count[10];  
+    int i, j, k, remainder, NOP=0, divisor=1, larger, pass;  
+    larger = largest(a);  
+    while(larger>0)  
+    {  
+        NOP++;  
+        larger/=10;  
+    }  
+    for(pass=0;pass<NOP;pass++) // Initialize the buckets  
+    {  
+        for(i=0;i<10;i++)  
+        bucket_count[i]=0;  
+        for(i=0;i<10;i++)  
+        {  
+            // sort the numbers according to the digit at passth place            
+            remainder = (a[i]/divisor)%10;  
+            bucket[remainder][bucket_count[remainder]] = a[i];  
+            bucket_count[remainder] += 1;  
+        }  
+        // collect the numbers after PASS pass  
+        i=0;  
+        for(k=0;k<10;k++)  
+        {  
+            for(j=0;j<bucket_count[k];j++)  
+            {  
+                a[i] = bucket[k][j];  
+                i++;  
+            }  
+        }  
+        divisor *= 10;  
+      
+}  
+}  
+*/
 
 int main()
 {
