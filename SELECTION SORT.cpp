38bcbe166ff549a3cd0110e76c0394c9a938e46b
KULDIP MOCHI <53702326+kulu123-z@users.noreply.github.com>
2019-11-22 00:57:27 +0530

    Update SELECTION SORT.cpp

--- SELECTION SORT.cpp
+++ SELECTION SORT.cpp
@@ -9,50 +9,34 @@ void swap(int *xp, int *yp)
 	*xp = *yp; 
 	*yp = temp; 
 } 
-/*int selectionSort(int arr[],int k, int n) 
+//this function will sort element 
+void selectionSort(int arr[], int size) 
 { 
-	int  j, min,pos; 
-
-		min = arr[k]; 
-		pos =k;
-		for (j = k+1; j < n; j++) 
-		{
-		
-			if (arr[j] < min) 
-			{
-				min = arr[j]; 
-				pos = j;
-			} 
-		}
-		return(pos);
-} */
-
-void selectionSort(int arr[], int n) 
-{ 
-	int i, j, min_idx; 
-
 	//total pass require for sorting? n-1
-	for (i = 0; i < n-1; i++) 
+	for (int i = 0; i < size-1; i++) 
 	{ 
 		
-		min_idx = i; //intially index of small element
+		int min_idx = i; //intially index of small element
 		
 		//now compare element from (i+1)th to nth element
-		for (j = i+1; j < n; j++) 
+		for (int j = i+1; j < n; j++) 
 		{
 			if (arr[j] < arr[min_idx]) //check condition for change small element index
 				min_idx = j; //now change index of small element
 		}
 		
-		// Swap the found minimum element with the first element 
-		swap(&arr[min_idx], &arr[i]); //call swap function
+		//check that if we require to change element
+		if(min_idx != i)
+		{
+			swap(&arr[min_idx], &arr[i]); //call swap function
+		}
 	} 
 } 
 
 //main funcion
 int main()
 {
-	int size,loc,temp;
+	int size;
 	cout<<"\nENTER SIZE OF ARRAY:";
 	cin>>size;
 	int arr[size];//array with user defined size
@@ -64,13 +48,7 @@ int main()
 	}
 	//call to selection sort
 	selectionSort(arr,size);
-/*	for(int i=0;i<=size-2;i++)
-	{
-		loc = selectionSort(arr, i, size);
-		temp = arr[i];
-		arr[i]= arr[loc];
-		arr[loc]= temp;
-	}*/
+
 	cout<<"\nSORTED ARRAY AFTER SELECION SORT: \n";
 	//print array element
 	for(int i=0;i<size;i++)
