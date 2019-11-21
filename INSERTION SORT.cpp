8de6c7d6e7d47f331220a356c49703c611ae1ca0
KULDIP MOCHI <53702326+kulu123-z@users.noreply.github.com>
2019-11-22 00:43:13 +0530

    Update INSERTION SORT.cpp

--- INSERTION SORT.cpp
+++ INSERTION SORT.cpp
@@ -1,30 +1,29 @@
 // C++ program for insertion sort 
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h> //input header file
 using namespace std; 
 
 //insertionSort function that will sort array element using insertion sort method
-void insertionSort(int arr[], int n) 
+void insertionSort(int arr[], int size) 
 { 
-	int i, key, j; 
+	
 	//here we assume that first element is on right place so loop start from second element
-	for (i = 1; i < n; i++) 
+	for (int i = 1; i < size; i++) //i represent first index of unsorted array
 	{ 
-		key = arr[i]; 
-		j = i - 1; 
+		int key = arr[i]; 
+		int j = i - 1; //j represent last index of sorted array
 		//compare all element whose index is less then key index and also key is less then compare element
 		while (j >= 0 && arr[j] > key) 
 		{ 
 			arr[j + 1] = arr[j]; 
-			j = j - 1; 
+			j--;
 		} 
 		arr[j + 1] = key; 
 	} 
 } 
 //print function that print array element
-void printArray(int arr[], int n) 
-{ 
-	int i; 
-	for (i = 0; i < n; i++) 
+void printArray(int arr[], int size) 
+{  
+	for (int i = 0; i < size; i++) 
 		cout << arr[i] << " "; 
 	cout << endl; 
 } 
@@ -32,24 +31,27 @@ void printArray(int arr[], int n)
 //main function
 int main()
 {
-	int size;
+	int size;//for input size
 	
 	cout<<"\nENTER SIZE OF ARRAY: ";
-	cin>>size;
+	cin>>size;//take input size
 	
-	int array[size];
+	int array[size];//array of input size
 	
+	//take input element
 	cout<<"\nENTER ELEMENT OF ARRAY: ";
 	for(int i=0; i<size; i++)
 		cin>>*(array + i);
 	
+	//print element
 	cout<<"\nARRAY WITHOUT SORT: ";
 	printArray(array, size);//call to printArray function
 	
+	//this will sort element in sorted form
 	insertionSort(array, size);//call to insertionSort function
 	
-	cout<<"\nSORTED ARRAY USING HEAP SORT: ";
+	//print element
+	cout<<"\nSORTED ARRAY USING INSERTION SORT: ";
 	printArray(array,size);
 }
  
-
